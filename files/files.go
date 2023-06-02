f188a2bace1bd2ef8497e8da4669640e22309f74
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2023-06-01 23:47:09 -0600

    Added imports to files.go

--- files/files.go
+++ files/files.go
@@ -1,1 +1,19 @@
 package files
+
+import (
+	"os"
+
+	"github.com/Xarasho/gofromzero/exercises"
+	//"bufio"
+	//"ioutils"
+)
+
+func StoreTable() {
+	var text string = exercises.MultiplyInput()
+	file, err := os.Create("./files/txt/table.txt")
+}
+
+func SumTable() {
+	var text string = exercises.MultiplyInput()
+
+}
